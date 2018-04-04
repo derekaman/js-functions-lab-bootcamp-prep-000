380bdb27656549b21199c169e7fac5a1c84e9ab1
Derek Aman <derekaman@gmail.com>
2018-04-04 21:52:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // write your code below!
 function happyHolidays() {
+<<<<<<< HEAD
   return ("Happy holidays!")
   }
 
@@ -13,4 +14,7 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
+=======
+  console.log("Happy holidays!");
+>>>>>>> a9d563e4470d1efb3a80a6452383a8f0f1dc5caf
 }
